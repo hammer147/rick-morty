52e2fa017ae3b48cb02243bf97383248e28e6817
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2022-10-29 13:42:13 +0200

    improved validation

--- src/server/trpc/router/rickandmorty.ts
+++ src/server/trpc/router/rickandmorty.ts
@@ -2,12 +2,13 @@ import { router, publicProcedure } from '../trpc'
 import { z } from 'zod'
 import { TRPCError } from '@trpc/server'
 
-// https://transform.tools/json-to-zod
+// https://github.com/colinhacks/zod#x-to-zod
+
 const rickandmortyValidator = z.object({
   info: z.object({
     count: z.number(),
     pages: z.number(),
-    next: z.string(),
+    next: z.string().nullable(),
     prev: z.string().nullable()
   }),
   results: z.array(
@@ -33,9 +34,9 @@ export const rickandmortyRouter = router({
     const rickandmortyRes = await fetch(`https://rickandmortyapi.com/api/character?page=${input}`)
     // error handling
     if (!rickandmortyRes.ok) {
-      // throw new TRPCError(...)
+      throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR', message: 'Something went wrong' })
     }
-    // validate the shape with Zod
+    // validate the shape with Zod (throws if invalid)
     const validated = rickandmortyValidator.parse(await rickandmortyRes.json())
     return validated
   })
