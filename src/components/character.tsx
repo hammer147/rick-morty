61ff929c6766c87d42022d1a7c971aeb74ca6e63
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2022-11-05 09:56:00 +0100

    improved styles

--- src/components/character.tsx
+++ src/components/character.tsx
@@ -5,15 +5,16 @@ type Props = { character: z.infer<typeof characterSchema> }
 
 export default function Character({ character }: Props) {
   return (
-    <div className='card'>
-      <img src={character.image} alt='' />
-      <div className='text-container'>
-        <h3>{character.name}</h3>
-        <p className='status'>
-          {character.status} - {character.species}
-        </p>
-        <p className='title'>Last seen on</p>
-        <p>{character.location.name}</p>
+    <div className='flex justify-center items-center m-2'>
+      <div className='flex flex-col items-center rounded-lg shadow-lg bg-white max-w-sm'>
+        <img className='m-3 rounded-t-lg' src={character.image} alt='' />
+        <div className='p-6'>
+          <h3 className='text-gray-900 text-xl font-medium mb-2'>{character.name}</h3>
+          <p className='text-gray-700 text-base mb-4'>
+            {character.status} - {character.species}
+          </p>
+          <p className='text-gray-700 text-base mb-2'>Last seen on {character.location.name}</p>
+        </div>
       </div>
     </div>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,11 +26,15 @@ const Home: NextPage = () => {
       {data.results.map(character => (
         <Character character={character} />
       ))}
-      <div>
-        <button onClick={() => setPage(old => Math.max(old - 1, 1))} disabled={page === 1}>
+      <div className='flex space-x-2 justify-center m-4'>
+        <button 
+        className='inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'
+        onClick={() => setPage(old => Math.max(old - 1, 1))} disabled={page === 1}>
           Previous
         </button>
-        <button onClick={() => setPage(old => old + 1)} disabled={!data.info.next}>
+        <button 
+        className='inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'
+        onClick={() => setPage(old => old + 1)} disabled={!data.info.next}>
           Next
         </button>
       </div>
